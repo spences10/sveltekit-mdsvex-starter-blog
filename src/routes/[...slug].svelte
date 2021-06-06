195d50dd1a7a8ec0fc8b84a9f8c68aa27a27d763
spences10 <spences10apps@gmail.com>
2021-06-06 22:06:11 +0100

    feat: :sparkles: add posts.json endpoint

--- src/routes/[...slug].svelte
+++ src/routes/[...slug].svelte
@@ -1,15 +1,10 @@
 <script context="module">
-  import { fromEntries, getAllPosts } from '$lib/util'
-  const files = import.meta.glob('../../posts/**/*.{md,svx}')
-
   export async function load(ctx) {
-    const posts = await getAllPosts(files)
-    const post = fromEntries(posts)[ctx.page.path]
-
     try {
       let ctxPath = ctx.page.path
       let index = ctxPath.split(`/`).length > 2 ? `/index` : ''
-      const Post = await import(`../../posts${ctxPath}${index}.md`)
+      let Post = await import(`../../posts${ctxPath}${index}.md`)
+
       return {
         props: {
           Post: Post.default,
