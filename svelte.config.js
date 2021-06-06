d8a2392e0f6fe9d85857ac524dbf74e588c3b429
spences10 <spences10apps@gmail.com>
2021-06-06 22:05:18 +0100

    feat: :package: add Svelte adapter-static

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
-import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
 import { mdsvex } from 'mdsvex'
+import preprocess from 'svelte-preprocess'
 import { mdsvexConfig } from './mdsvex.config.js'
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
@@ -13,6 +15,7 @@ const config = {
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
     target: '#svelte',
+    adapter: adapter(),
   },
 }
 
