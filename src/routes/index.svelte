54f6affa58cae09cb860d6a8a05e249d4e76f438
spences10 <spences10apps@gmail.com>
2021-09-16 22:25:41 +0100

    feat: :art: add index hero

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
-  documentation
-</p>
+<script>
+  import EndlessClouds from '$lib/components/endless-clouds.svelte'
+  import { description, siteName } from '$lib/site-config'
+</script>
+
+<div class="hero min-h-[65vh] mb-20">
+  <EndlessClouds />
+  <div class="hero-overlay bg-opacity-75" />
+  <div class="text-center hero-content text-neutral-content">
+    <div class="max-w-2xl break-all">
+      <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
+      <h2 class="mb-5 text-4xl font-bold">
+        Welcome to the {siteName}
+      </h2>
+      <p class="mb-5 break-words">
+        {description}
+      </p>
+      <a href={`/posts`} class="btn btn-primary"
+        >Check Out The Posts Page</a
+      >
+    </div>
+  </div>
+</div>
