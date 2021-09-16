35f750993fbc1c2fc6e66b2e8c6cd0f9667f4665
spences10 <spences10apps@gmail.com>
2021-09-16 22:25:17 +0100

    feat: :sparkles: add site config file

---
+++ src/lib/site-config.js
@@ -0,0 +1,17 @@
+export const siteName = 'The SvelteKit Blog'
+
+export const website = `https://thesveltekit.com`
+
+export const description = `This project is built with SvelteKit, MDSveX, Tailwind and DaisyUI`
+
+export const pages = [
+  { title: `Home`, path: `/` },
+  { title: `Posts`, path: `/posts` },
+  { title: `About`, path: `/about` },
+]
+
+export const socialLinks = [
+  { title: `Twitter`, path: `https://twitter.com/username` },
+  { title: `YouTube`, path: `https://youtube.com/username` },
+  { title: `GitHub`, path: `https://github.com/username` },
+]
