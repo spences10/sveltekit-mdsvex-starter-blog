1d9fbd49147bd744e7d7ae7c28d2c632675f10f2
spences10 <spences10apps@gmail.com>
2021-09-16 22:24:57 +0100

    feat: :sparkles: components for hereo and post card

---
+++ src/lib/components/post-card.svelte
@@ -0,0 +1,29 @@
+<script>
+  import truncateHtml from 'truncate-html'
+
+  export let post
+
+  let excerpt = truncateHtml(post.html, 300, {
+    ellipsis: '...',
+  })
+</script>
+
+<article
+  class="bg-base-100 mb-10 p-5 transition border border-primary shadow-xl card hover:text-accent-focus first:pt-0"
+>
+  <a sveltekit:prefetch href={`/posts/${post.slug}`}>
+    <div>
+      <h2 class="font-black mb-1 text-3xl mt-5">
+        {post.title}
+      </h2>
+      <div class="mb-4 text-accent uppercase text-sm font-bold">
+        <time>{Date(post.date)}</time>
+        •
+        <span>{post.readingTime.text}</span>
+      </div>
+    </div>
+    <div class="all-prose">
+      {@html excerpt}
+    </div>
+  </a>
+</article>
