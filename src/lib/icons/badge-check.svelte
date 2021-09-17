963d5ba7c8ac07c57d39a7c3d834e83f57fb448d
spences10 <spences10apps@gmail.com>
2021-09-17 07:49:30 +0100

    feat: :sparkles: add badge check to footer

---
+++ src/lib/icons/badge-check.svelte
@@ -0,0 +1,22 @@
+<script>
+  export let height = 20
+  export let width = 20
+  export let tailwindClasses = ''
+</script>
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+  viewBox="0 0 24 24"
+  stroke="currentColor"
+  {height}
+  {width}
+  class={tailwindClasses}
+>
+  <path
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    stroke-width="2"
+    d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z"
+  />
+</svg>
