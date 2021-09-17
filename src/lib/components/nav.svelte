3aae0f37a47543c897011c95e893c526f9d12ca5
spences10 <spences10apps@gmail.com>
2021-09-17 09:13:55 +0100

    feat: :sparkles: add responsive nav items

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -4,10 +4,23 @@
 </script>
 
 <div
-  class="navbar mb-2 shadow-lg bg-neutral text-neutral-content rounded-box mb-16"
+  class="navbar mb-2 shadow-lg bg-neutral text-neutral-content mb-16 sticky top-0 z-10"
 >
   <div class="px-2 mx-2 navbar-start">
     <span class="text-lg font-bold">{siteName}</span>
+  </div>
+  <div class="dropdown lg:hidden dropdown-right">
+    <div tabindex="0" class="m-1 btn">Links</div>
+    <ul
+      tabindex="0"
+      class="p-2 shadow menu dropdown-content bg-neutral text-neutral-content rounded-box w-52 "
+    >
+      {#each pages as { title, path }}
+        <li>
+          <a href={path}>{title}</a>
+        </li>
+      {/each}
+    </ul>
   </div>
   <div class="hidden px-2 mx-2 navbar-center lg:flex">
     <div class="flex items-stretch">
