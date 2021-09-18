71ff415a08b97f37420768912e3db2dd99058b82
spences10 <spences10apps@gmail.com>
2021-09-18 15:35:57 +0100

    refactor: :recycle: remove double adapter

--- svelte.config.js
+++ svelte.config.js
@@ -7,19 +7,18 @@ import mdsvexConfig from './mdsvex.config.js'
 const config = {
   extensions: ['.svelte', ...mdsvexConfig.extensions],
 
-  kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
-    adapter: adapter(),
-    adapter: adapter(),
-  },
-
   preprocess: [
     mdsvex(mdsvexConfig),
     preprocess({
       postcss: true,
     }),
   ],
+
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: adapter(),
+  },
 }
 
 export default config
